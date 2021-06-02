390238c036901117717122991314af2817635fd3
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-06-02 09:33:34 -0300

    Add files via upload
    Currying

---
+++ Javascript Avançado M1 IGTI/Aula2.6/index.js
@@ -0,0 +1,24 @@
+//EXEMPLO DE CURRYING
+
+function log(date, type, message) {
+
+    console.log(`[${date.getHours()}:${date.getMinutes()}] [${type}] ${message}`);
+}
+
+log(new Date(), "DEBUG", "Exemplo de currying");
+
+
+const logCurrying = date => type => message => { console.log(`[${date.getHours()}:${date.getMinutes()}] [${type}] ${message}`);
+}
+
+logCurrying(new Date())("DEBUG")("Exemplo de currying");
+
+
+let logNow = logCurrying(new Date());
+
+logNow("DEBUG")("Exemplo de currying com paramentro fixo");
+
+
+let logDebugNow = logNow("DEBUG");
+
+logDebugNow("Exemplo de nova funcao por currying com paramentros fixo")
