f913bb3bbd61554f9018edf890abc682868e177e
Jean Michael <73749746+J3anMichael@users.noreply.github.com>
2021-06-15 12:07:12 -0300

    Add files via upload

---
+++ React-Countries/src/components/Item.jsx
@@ -0,0 +1,8 @@
+export default function Item({ children: value, label = "Nome:" }) {
+  return (
+    <span className="text-sm">
+      <strong>{label}</strong> {""}
+      {value}
+    </span>
+  );
+}
